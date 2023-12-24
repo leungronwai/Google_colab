3a8fd03127b666687ffda1176cb5893e64afe3c9
leungronwai <41854127+leungronwai@users.noreply.github.com>
2023-12-24 14:01:43 +0800

    在abc中添加内容

---
+++ Test/Acb.py
@@ -0,0 +1,14 @@
+# 使用方法：
+
+from Acb import Acb
+
+# 实例化一个Acb对象
+acb = Acb()
+
+# 调用方法
+print(acb.get_acbs())
+print(acb.get_acbs(1))
+print(acb.get_acbs(1, 2))
+print(acb.get_acbs(1, 2, 3))
+print(acb.get_acbs(1, 2, 3, 4))
+print(acb.get_acbs(1, 2, 3, 4, 5))
